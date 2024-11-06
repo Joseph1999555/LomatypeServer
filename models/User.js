faf63af584acd418cf92bdabdf6849a403bac728
nattipan jarunat <nattipanjarunat@gmail.com>
2024-11-07 01:22:13 +0700

    add vercel.json3

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); // Import bcrypt
+const bcrypt = require('bcryptjs'); // Import bcrypt
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true},
